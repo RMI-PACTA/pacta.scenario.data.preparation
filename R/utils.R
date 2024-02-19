49b91d3bb79e48183a59e5087f07e5f5b0302886
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-19 11:46:39 +0100

    gains `standardized_scenario_columns`
    from `data-raw`

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+standardized_scenario_columns <- function() {
+  c(
+    "source",
+    "scenario",
+    "scenario_geography",
+    "sector",
+    "technology",
+    "indicator",
+    "units",
+    "year",
+    "value"
+  )
+}

65300c011ee31054f02d0a0cf33d95c22760e136
CJ Yetman <cj@cjyetman.com>
2024-03-28 09:02:46 +0100

    add `prepare_geco_2023_scenario()` (#21)
    * add `prepare_geco_2023_scenario()`
    
    * change dplyr pronoun syntax
    
    * integrate `geco_2023_technology_bridge`
    
    * implement `geco_2023_geography_bridge`
    
    * minor spelling fix
    
    * add tests (only with local `devtools::test()`) for `geco_2023_geography_bridge` and `geco_2023_technology_bridge`
    
    * use better column names for `geco_2023_technology_bridge`
    
    * only use `geco_2023_technology_bridge` where needed
    
    * make `geco_2023_geography_bridge` and `geco_2023_technology_bridge` internal
    
    * reduce `geco_2023_technology_bridge.csv` to essentials
    
    * reduce `geco_2023_technology_bridge.csv` to minimum
    
    * alphabetize and remove quotes from `geco_2023_geography_bridge.csv`
    
    * remove quotes from `geco_2023_technology_bridge.csv`

---
+++ tests/testthat/test-geco_2023_technology_bridge.R
@@ -0,0 +1,17 @@
+test_that("exported data matches raw data", {
+  skip_if(nzchar(Sys.getenv("R_CMD")), "Not run in R CMD check")
+  raw_data <-
+    readr::read_csv(
+      file = here::here("data-raw/geco_2023_technology_bridge.csv"),
+      na = "",
+      col_types = readr::cols(
+        scenario_technology_name = "c",
+        standardized_technology_name = "c"
+      )
+    )
+
+  expect_equal(
+    geco_2023_technology_bridge,
+    raw_data
+  )
+})

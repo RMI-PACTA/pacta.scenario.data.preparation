6b4d6106289ac7cc9ff514e29e306b3bf2807b58
CJ Yetman <cyetman@rmi.org>
2024-04-15 13:53:35 +0200

    export `scenario_regions` and `scenario_source_pacta_geography_bridge`

--- data-raw/prepare_raw_data.R
+++ data-raw/prepare_raw_data.R
@@ -1,7 +1,10 @@
 raw_data_env <- new.env()
 
-source(file = "data-raw/prepare_scenario_regions.R", local = raw_data_env)
-source(file = "data-raw/prepare_scenario_source_pacta_geography_bridge.R", local = raw_data_env)
+source(file = "data-raw/prepare_scenario_regions.R")
+usethis::use_data(scenario_regions, overwrite = TRUE, version = 3)
+
+source(file = "data-raw/prepare_scenario_source_pacta_geography_bridge.R")
+usethis::use_data(scenario_source_pacta_geography_bridge, overwrite = TRUE, version = 3)
 
 source(file = "data-raw/prepare_geco_2022_geography_bridge.R", local = raw_data_env)
 source(file = "data-raw/prepare_geco_2022_technology_bridge.R", local = raw_data_env)

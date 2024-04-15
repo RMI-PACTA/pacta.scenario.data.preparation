9996abababac2ca4dd009e3cfc211cb7ca1fc681
CJ Yetman <cyetman@rmi.org>
2024-04-15 12:11:13 +0200

    port changes to `add_market_share_columns()` from `pacta.scenario.preparation`

--- R/add_market_share_columns.R
+++ R/add_market_share_columns.R
@@ -20,44 +20,56 @@ add_market_share_columns <- function(data, reference_year) {
   if (!(reference_year %in% data$year)) {
     rlang::abort(
       glue::glue(
-        "Data must contain reference year: {reference_year}."
-      ),
+        "Data must contain reference year: {reference_year}."),
       class = "missing_year"
     )
   }
 
   data <- dplyr::filter(data, data$year >= reference_year)
-  data <- add_technology_fair_share_ratio(data)
-  data <- add_market_fair_share_percentage(data)
 
-  dplyr::group_by(data, !!!old_groups)
+  data %>%
+    add_technology_fair_share_ratio() %>%
+    add_market_fair_share_percentage() %>%
+    dplyr::group_by(!!!old_groups)
 }
+
 
 add_technology_fair_share_ratio <- function(data) {
-  data <- dplyr::ungroup(data)
-  data <- dplyr::group_by(data, !!!rlang::syms(c(common_fs_groups(), "technology")))
-  data <- dplyr::arrange(data, data$year, .by_group = TRUE)
-  data <- dplyr::mutate(data, tmsr = (data$value - dplyr::first(data$value)) / dplyr::first(data$value))
-
-  # if both the first value and the value at time t are 0, the resulting TMSR is NaN.
-  # to avoid issues in calculations later on, we set tmsr = 0 in that case,
-  # which implies no change. Note that this will be returned as a ratio of 1,
-  # based on format_p4b(), but the tmsr is a plain rate of change at this point
-  data <- dplyr::mutate(data, tmsr = ifelse(data$value == 0 & dplyr::first(data$value) == 0, 0, data$tmsr))
-
-  dplyr::ungroup(data)
+  data %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(!!!rlang::syms(c(common_fs_groups(), "technology"))) %>%
+    dplyr::arrange(.data$year, .by_group = TRUE) %>%
+    dplyr::mutate(tmsr = (.data$value - dplyr::first(.data$value)) / dplyr::first(.data$value)) %>%
+    # if both the first value and the value at time t are 0, the resulting TMSR is NaN.
+    # to avoid issues in calculations later on, we set tmsr = 0 in that case,
+    # which implies no change. Note that this will be returned as a ratio of 1,
+    # based on format_p4b(), but the tmsr is a plain rate of change at this point
+    dplyr::mutate(
+      tmsr = dplyr::if_else(
+        .data$value == 0 & dplyr::first(.data$value) == 0,
+        0,
+        .data$tmsr
+      )
+    ) %>%
+    dplyr::ungroup()
 }
+
 
 add_market_fair_share_percentage <- function(data) {
-  data <- dplyr::ungroup(data)
-  data <- dplyr::group_by(data, !!!rlang::syms(c(common_fs_groups(), "year")))
-  data <- dplyr::arrange(data, data$year, .by_group = TRUE)
-  data <- dplyr::mutate(data, sector_total_by_year = sum(data$value))
-  data <- dplyr::group_by(data, !!!rlang::syms(c(common_fs_groups(), "technology")))
-  data <- dplyr::mutate(data, smsp = (data$value - dplyr::first(data$value)) / dplyr::first(data$sector_total_by_year))
-
-  dplyr::ungroup(data)
+  data %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(!!!rlang::syms(c(common_fs_groups(), "year"))) %>%
+    dplyr::arrange(.data$year, .by_group = TRUE) %>%
+    dplyr::mutate(sector_total_by_year = sum(.data$value)) %>%
+    dplyr::group_by(!!!rlang::syms(c(common_fs_groups(), "technology"))) %>%
+    dplyr::mutate(
+      smsp = (.data$value - dplyr::first(.data$value)) /
+        dplyr::first(.data$sector_total_by_year),
+      sector_total_by_year = NULL
+    ) %>%
+    dplyr::ungroup()
 }
+
 
 common_fs_groups <- function() {
   c("scenario", "sector", "scenario_geography")
